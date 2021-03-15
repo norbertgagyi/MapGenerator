f241691ec58df7354c7eef077801a5d74578955a
Norbert Gagyi <norbert.gagyi@gmail.com>
2021-03-15 13:53:30 +0200

    first commit add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MapGeneratorV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(893, 364);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(114, 17);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(114, 43);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(114, 69);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(114, 95);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown4.TabIndex = 5;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 369);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(413, 122);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Map properties";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(270, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Seed";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Use custom seed:";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fill amount (percent):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit size (pixels):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y size (units):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X size (units):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 494);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MapGenerator v3.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapGeneratorV3
+{
+    public partial class Form1 : Form
+    {
+        public struct Tile
+        {
+            public int x, y, type;
+        };
+
+        Tile[,] tileMap = new Tile[200, 200];
+
+        List<Tile> tiles = new List<Tile>();
+
+        Graphics graphics;
+
+        SolidBrush[] brushes = { new SolidBrush(Color.White), new SolidBrush(Color.DarkBlue), new SolidBrush(Color.Red) };
+
+        Pen pen = new Pen(Color.Black);
+
+        Random random;
+
+        public int tileSize;
+
+        public int xCount;
+
+        public int yCount;
+
+        public int fillPercent;
+
+        public bool useCustomSeed;
+
+        public void UpdateInformation()
+        {
+            tileSize = (int)numericUpDown1.Value;
+            xCount = (int)numericUpDown2.Value;
+            yCount = (int)numericUpDown3.Value;
+            fillPercent = (int)numericUpDown4.Value;
+            useCustomSeed = checkBox1.Checked;
+        }
+
+        public void WriteMapTilesToList()
+        {
+            tiles.Clear();
+
+            for (int x = 0; x < xCount; x++)
+                for (int y = 0; y < yCount; y++)
+                    tiles.Add(tileMap[x, y]);
+        }
+
+        List<Tile> fillDrawnTiles = new List<Tile>();
+
+        public void Fill(int x, int y, int replaceWhat, int replaceWith)
+        {
+            if(x >= 0 && x < xCount && y >= 0 && y < yCount && tileMap[x,y].type == replaceWhat)
+            {
+                tileMap[x, y].type = replaceWith;
+                fillDrawnTiles.Add(tileMap[x, y]);
+
+                Fill(x + 1, y, replaceWhat, replaceWith);
+                Fill(x - 1, y, replaceWhat, replaceWith);
+                Fill(x, y + 1, replaceWhat, replaceWith);
+                Fill(x, y - 1, replaceWhat, replaceWith);
+            }
+
+            WriteMapTilesToList();
+        }
+
+        public void GenerateMap(int seed, int _fillPercent)
+        {
+            random = new Random(seed);
+
+            for (int x = 0; x < xCount; x++)
+                for (int y = 0; y < yCount; y++)
+                {
+                    int randomNumber = random.Next(0, 100) < _fillPercent ? 1 : 0;
+
+                    tileMap[x, y].x = x * (tileSize + 1);
+                    tileMap[x, y].y = y * (tileSize + 1);
+                    tileMap[x, y].type = randomNumber;
+                }
+
+            WriteMapTilesToList();
+        }
+
+        public void DrawTiles(List<Tile> _tiles)
+        {
+            graphics = panel1.CreateGraphics();
+
+            foreach (Tile a in _tiles)
+            {
+                graphics.FillRectangle(brushes[a.type], a.x, a.y, tileSize, tileSize);
+            }
+        }
+
+        public void DrawGrid()
+        {
+            graphics.DrawLine(pen, 0, 0, (tileSize + 1) * xCount - 1, 0);
+            graphics.DrawLine(pen, 0, 0, 0, (tileSize + 1) * yCount - 1);
+
+            for (int x = 0; x <= xCount; x++)
+                graphics.DrawLine(pen, (tileSize + 1) * x - 1, 0, (tileSize + 1) * x - 1, (tileSize + 1) * yCount - 1);
+
+            for (int y = 0; y <= yCount; y++)
+                graphics.DrawLine(pen, 0, (tileSize + 1) * y - 1, (tileSize + 1) * xCount - 1, (tileSize + 1) * y - 1);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateInformation();
+
+            int seed = useCustomSeed == true ? textBox1.Text.GetHashCode() : Environment.TickCount;
+
+            GenerateMap(seed, fillPercent);
+            DrawTiles(tiles);  
+                      
+            DrawGrid();
+                        
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point cursorXY = panel1.PointToClient(Cursor.Position);
+
+            fillDrawnTiles.Clear();
+            Fill(cursorXY.X / (tileSize + 1), cursorXY.Y / (tileSize + 1), 0, 2);
+            DrawTiles(fillDrawnTiles);
+
+            DrawGrid();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+        }
+    }
+}
